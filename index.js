73d45b86a2cb8fa8ec621b1fb258898a3b9be2c0
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2023-07-20 20:28:46 +0530

    --

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import connectToMongo from './db.js';
 import express from 'express';
 import cors from 'cors';
-// import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -20,10 +19,6 @@ import notesRoutes from './routes/notes.js';
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', notesRoutes);
 
-// const __mydirname = path.resolve();
-// console.log("MY dirname is", __mydirname)
-// console.log(process.env.NODE_ENV);
-// console.log(process.env.PORT);
 
 if(process.env.NODE_ENV === 'production'){
         app.get("/",(req,res)=>{
